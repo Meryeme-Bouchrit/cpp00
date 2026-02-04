f19f2ae9923d155ba772e9e96f1f76052b465ae8
Meryeme Bouchrit <bouchritmeryeme08@gmail.com>
2026-02-04 06:29:33 +0000

    update + ex01 start

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -7,12 +7,16 @@ int main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	for (int i = 1; i < ac; i++)
+	int i = 1;
+	while (i < ac)
     {
-        for (int j = 0; av[i][j]; j++)
+        int j = 0;
+        while (av[i][j])
         {
             std::cout << (char)std::toupper(av[i][j]);
+            j++;
         }
+        i++;
     }
     std::cout << std::endl;
     return 0;
