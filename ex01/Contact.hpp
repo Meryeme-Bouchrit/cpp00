e168b641175d2ec0b27aa7bfa9ca4ba18512e564
Meryeme Bouchrit <bouchritmeryeme08@gmail.com>
2026-02-05 23:48:46 +0000

    update

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -2,23 +2,30 @@
 #define CONTACT_HPP
 
 #include <string>
+#include <cctype>
+
 class Contact {
+
 private:
     std::string firstName;
     std::string lastName;
     std::string nickname;
     std::string phoneNumber;
     std::string darkestSecret;    
+
 public:
-  
     void setFirstName(std::string firstName);
     void setLastName(std::string lastName);
     void setNickname(std::string nickname);
     void setPhoneNumber(std::string phoneNumber);
     void setDarkestSecret(std::string darkestSecret);
-    
-    
-    // Getters - what return type? should they be const?
+
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getPhoneNumber() const;
+    std::string getDarkestSecret() const;
+
 };
 
 #endif
